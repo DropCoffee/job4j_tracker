fbd17f97630cab12756d56e30f03a5b1599236cd
DropCoffee <malyukov.maksim90@yandex.ru>
2024-05-10 18:14:07 +0300

    1.5. Поля объекта [#166083]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -1,16 +1,36 @@
 package ru.job4j.oop;
 
 public class Cat {
+
+    private String name;
+
+    private String food;
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
     public String sound() {
         String voice = "may-may";
         return voice;
     }
 
+    public void show() {
+        System.out.println("Имя котенка: " + this.name + ", ел " + this.food);
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
     public static void main(String[] args) {
         Cat peppy = new Cat();
         Cat sparky = new Cat();
-
         String say = peppy.sound();
         System.out.println("Peppy says " + say);
+
+        peppy.eat("kotleta");
+        peppy.giveNick("Gav");
+        peppy.show();
     }
 }
