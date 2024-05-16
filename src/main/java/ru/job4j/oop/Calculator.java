16709f6b5dfc6c5ecc4cde76b85ec3a53dd5fc9f
DropCoffee <malyukov.maksim90@yandex.ru>
2024-05-16 11:42:00 +0300

    1.9. Статические и нестатические методы [#504806 #496093]

--- src/main/java/ru/job4j/oop/Calculator.java
+++ src/main/java/ru/job4j/oop/Calculator.java
@@ -21,8 +21,7 @@ public class Calculator {
     }
 
     public int sumAllOperation(int a) {
-        Calculator calculator = new Calculator();
-        return Calculator.minus(a) + Calculator.sum(a) + calculator.multiply(a) + calculator.divide(a);
+        return Calculator.minus(a) + Calculator.sum(a) + multiply(a) + divide(a);
     }
 
     public static void main(String[] args) {
