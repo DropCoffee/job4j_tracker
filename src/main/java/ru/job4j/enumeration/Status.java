677f48bb2bedaa17bedfedb2948aab52533a767d
DropCoffee <malyukov.maksim90@yandex.ru>
2024-05-23 11:42:30 +0300

    12. Enum  [#504960]

---
+++ src/main/java/ru/job4j/enumeration/Status.java
@@ -0,0 +1,63 @@
+package ru.job4j.enumeration;
+
+public enum Status implements Code {
+    ACCEPTED(1, "Принят") {
+        private String message = "Автомобиль принят на СТО";
+
+        @Override
+        public String getMessage() {
+            return message;
+        }
+
+    },
+    IN_WORK(2, "В работе") {
+        private String message = "Автомобиль в работе";
+
+        @Override
+        public String getMessage() {
+            return message;
+        }
+    },
+    WAITING(3, "Ожидает клиента") {
+        private String message = "Автомобиль ожидает запчасти";
+
+        @Override
+        public String getMessage() {
+            return message;
+        }
+    },
+    FINISHED(4, "Завершен") {
+        private String message = "Все работы завершены";
+
+        @Override
+        public String getMessage() {
+            return message;
+        }
+    };
+
+    private final String text;
+    private final int code;
+
+    Status(int code, String text) {
+        this.text = text;
+        this.code = code;
+    }
+
+    public String info() {
+        return text;
+    }
+
+    public static void main(String[] args) {
+        Status toyota = Status.FINISHED;
+        Status volvo = Status.WAITING;
+        System.out.println("Статус Toyota: " + toyota.info());
+        System.out.println("Статус Volvo: " + volvo.info());
+    }
+
+    @Override
+    public int code() {
+        return code;
+    }
+
+    public abstract String getMessage();
+}
