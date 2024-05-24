6f96650e87543beeb7037bb9c6f81d75d4c4e15b
DropCoffee <malyukov.maksim90@yandex.ru>
2024-05-24 20:00:01 +0300

    4. Телефонный справочник на базе ArrayList [#41598 #497263] упрощение

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -22,13 +22,15 @@ public class PhoneDictionary {
 
     /**
      * Вернуть список всех пользователей, который содержат key в любых полях.
+     *
      * @param key Ключ поиска.
      * @return Список пользователей, которые прошли проверку.
      */
     public ArrayList<Person> find(String key) {
         ArrayList<Person> result = new ArrayList<>();
         for (Person person : persons) {
-            if (findByKey(key, person.getName(), person.getSurname(), person.getAddress(), person.getPhone())) {
+            if (key.equals(person.getName()) || key.equals(person.getSurname())
+                    || key.equals(person.getPhone()) || key.equals(person.getAddress())) {
                 result.add(person);
             }
         }
