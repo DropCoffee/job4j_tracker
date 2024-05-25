4f273cdcf14ea4a8c6a30bafdd5a444b0a070aa0
DropCoffee <malyukov.maksim90@yandex.ru>
2024-05-25 10:30:59 +0300

    3. Автоупаковка / автораспаковка [#504971]

---
+++ src/main/java/ru/job4j/box/Box.java
@@ -0,0 +1,12 @@
+package ru.job4j.box;
+
+public class Box {
+
+    public static void main(String[] args) {
+        int a = 1;
+        Integer b;
+        b = a;
+        System.out.println("Integer b = " + b);
+    }
+
+}
