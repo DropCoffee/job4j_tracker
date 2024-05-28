34a758479a4edc4a289f2bad5aceb5ba8df5cb2a
DropCoffee <malyukov.maksim90@yandex.ru>
2024-05-28 22:32:03 +0300

    11.0. Шаблон проектирования - Стратегия [#786 #498096] исправление отрисовки квадрата

--- src/main/java/ru/job4j/strategy/Square.java
+++ src/main/java/ru/job4j/strategy/Square.java
@@ -3,6 +3,10 @@ package ru.job4j.strategy;
 public class Square implements Shape {
     @Override
     public String draw() {
-        return String.valueOf((char) 11036);
+        String ln = System.lineSeparator();
+        return "----" + ln
+                + "|  |" + ln
+                + "|  |" + ln
+                + "----" + ln;
     }
 }
