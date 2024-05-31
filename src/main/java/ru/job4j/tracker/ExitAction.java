9ba2aec031ea8955b9358ae982440bcb907fcf34
DropCoffee <malyukov.maksim90@yandex.ru>
2024-05-31 08:23:32 +0300

    12.1 Зависимость от System.out [#33568]

--- src/main/java/ru/job4j/tracker/ExitAction.java
+++ src/main/java/ru/job4j/tracker/ExitAction.java
@@ -1,6 +1,12 @@
 package ru.job4j.tracker;
 
 public class ExitAction implements UserAction {
+    private final Output output;
+
+    public ExitAction(Output output) {
+        this.output = output;
+    }
+
     @Override
     public String name() {
         return "Завершить программу";
@@ -8,6 +14,7 @@ public class ExitAction implements UserAction {
 
     @Override
     public boolean execute(Input input, Tracker tracker) {
+        output.println("=== Завершение программы ===");
         return false;
     }
 }
