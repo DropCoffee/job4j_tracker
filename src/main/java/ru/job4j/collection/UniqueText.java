f847a0e05c43df36313c93bc0b27268d70bde606
DropCoffee <malyukov.maksim90@yandex.ru>
2024-06-08 08:59:36 +0300

    2. Идентичные тексты [#212655]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,22 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public boolean isEquals(String originText, String duplicateText) {
+        boolean result = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        for (String word : origin) {
+            check.add(word);
+        }
+        for (String checkWord : text) {
+            if (!check.contains(checkWord)) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
