aed573ef86ec9815e692b911ce6d3b872bd227be
DropCoffee <malyukov.maksim90@yandex.ru>
2024-06-10 20:08:50 +0300

    0. Map, HashMap [#212652 #499933]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,7 +6,9 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("parsentev@yandex.ru", "Petr Arsentev");
-        map.put("testv@yandex.ru", "Bob");
+        map.put("test@yandex.ru", "Bob");
+        map.put("test@yandex.ru", "Sam");
+        map.put("alex@gmail.ru", "Alex");
         map.remove("parsentev@yandex.ru");
         for (String key : map.keySet()) {
             String value = map.get(key);
